95888e5e164d07dd4e7076c642798486258d5871
abhxi-r-s <abhishekunni8281@gmail.com>
2025-05-08 10:04:18 +0530

    arraylist

---
+++ programs/array2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class array2 {
+
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        // ArrayList <int> List= new ArrayList<int>();
+        //By default <T>
+
+        ArrayList <String> List2= new ArrayList<String>();
+
+        List2.add("English");
+        System.out.println(List2);
+
+        sc.close();
+    }
+    
+}
